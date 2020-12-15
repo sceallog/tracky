f380b0a7dcf80a0bf1550beb3e9bbefd016d3777
Sven Buettner <sven.buettner1@gmail.com>
2020-12-15 19:43:13 +0900

    Add Devise, Bootstrap, a theme, and a User model

---
+++ app/javascript/packs/vendor.js
@@ -0,0 +1,1 @@
+import 'vendor/jquery.easing.min.js'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register', sign_up: 'signup' }
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
