d06214c378845e22be4d55ae6826691addbe5634
Sven Buettner <sven.buettner1@gmail.com>
2021-02-23 15:33:10 +0900

    And another time

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+module.exports = {
+  environment.toWebpackConfig(),
+  resolve: {
+    modules: [
+      //path.join(__dirname, "js/helpers"),
+      "node_modules"
+    ]
+  }
+};
